9edcba41a5db12622f758f4c00c6fc1e359f98a1
Aravind <aravindkn@outlook.com>
2025-09-02 22:27:16 +0200

    barebones implementation of tcp

--- Nexus.Client/Program.cs
+++ Nexus.Client/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+using var client = new TcpClient();
+await client.ConnectAsync("127.0.0.1", 9000);
+Console.WriteLine("Connected to server!");
+
+using var stream = client.GetStream();
+
+_ = Task.Run(async () =>
+{
+    var buffer = new byte[4096];
+    while (true)
+    {
+        var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+        if (bytesRead == 0) break;
+        var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        Console.WriteLine($"Server response: {message}");
+    }
+});
+
+
+while (true)
+{
+    Console.Write("Enter message to send: ");
+    var line = Console.ReadLine();
+    if (string.IsNullOrEmpty(line)) continue;
+    
+    var messageBytes = Encoding.UTF8.GetBytes(line);
+    await stream.WriteAsync(messageBytes);
+}
--- Nexus.Server/Program.cs
+++ Nexus.Server/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var listener = new TcpListener(IPAddress.Any, 9000);
+listener.Start();
+Console.WriteLine("Server started on port 9000...");
+
+using var client = await listener.AcceptTcpClientAsync();
+Console.WriteLine("Client connected!");
+
+using var stream = client.GetStream();
+var buffer = new byte[4096]; // A buffer to read data into.
+
+while (true)
+{
+    var bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+    if (bytesRead == 0) // The client has disconnected.
+    {
+        Console.WriteLine("Client disconnected.");
+        break;
+    }
+    
+    var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+    Console.WriteLine($"Received: {message}");
+
+    await stream.WriteAsync(buffer, 0, bytesRead);
+    Console.WriteLine($"Echoed: {message}");
+}
